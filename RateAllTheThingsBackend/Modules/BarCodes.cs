7d3eaca3bdbfb54e5dc3c1195705d80fa373c498
Fredrik Leijon <fredrik.leijon@gmail.com>
2012-01-09 19:29:09 +0100

    switched to nancy

---
+++ RateAllTheThingsBackend/Modules/BarCodes.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+using RateAllTheThingsBackend.Repositories;
+
+namespace RateAllTheThingsBackend.Modules
+{
+    public class BarCodes:NancyModule
+    {
+        private readonly IBarCodes barcodes;
+
+        public BarCodes(IBarCodes barcodes)
+        {
+            this.barcodes = barcodes;
+
+            Get["/BarCode"] = x =>
+                                  {
+                                      return Response.AsJson(this.barcodes.All());
+                                  };
+
+            Get["/BarCode/{format}/{code}"] = x =>
+                                {
+                                    var barcode = this.barcodes.Get(x.format, x.code);
+                                    return Response.AsJson(new[] { barcode });
+                                };
+        }
+    }
+}
