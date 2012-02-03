eba17bdb9161d917bd713df491426bd9a0face37
Fredrik Leijon <fredrik.leijon@gmail.com>
2012-02-03 19:15:51 +0100

    Added auth method

---
+++ RateAllTheThingsBackend/Modules/AuthModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using Nancy.Security;
+
+namespace RateAllTheThingsBackend.Modules
+{
+    public class AuthModule : NancyModule
+    {
+        public AuthModule(): base("/Auth")
+        {
+            this.RequiresAuthentication();
+
+            Get["/"] = x =>
+                           {
+                               return Response.AsJson(new[] {true});
+                           };
+        }
+    }
+}
